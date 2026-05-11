155bda1da87b59fd4d717b1b752965f62946b56c
psyUIX <dejanpesut.dv@gmail.com>
2026-05-11 20:37:55 +0200

    Dodat React u sve komponente

---
+++ src/pages/About.jsx
@@ -0,0 +1,30 @@
+import { Link, Outlet, useLocation } from 'react-router-dom';
+import './About.css';
+
+function About() {
+  const location = useLocation();
+
+  return (
+    <div className="about">
+      <h1>O aplikaciji</h1>
+      <p className="about-intro">Izaberite šta vas zanima:</p>
+      <nav className="about-nav">
+        <Link
+          to="/about/app"
+          className={`about-link ${location.pathname === '/about/app' ? 'active' : ''}`}
+        >
+          O aplikaciji
+        </Link>
+        <Link
+          to="/about/author"
+          className={`about-link ${location.pathname === '/about/author' ? 'active' : ''}`}
+        >
+          O autoru
+        </Link>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
+
+export default About;
